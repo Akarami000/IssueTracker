377e33b427e8966f74379dd9b152059060dfe6d8
Akshay Pariyar <akaramiakshay@gmail.com>
2025-06-16 23:52:29 +0530

    fixing process for CI

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,18 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
   pluginReact.configs.flat.recommended,
 ]);
